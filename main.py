de5948800fa7307c5153cc79e5c543fe3dd6ea69
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2021-06-11 11:10:35 -0600

    Remove referral part from url that the tracker checks with.  Make switch higher priority in searching

--- main.py
+++ main.py
@@ -28,6 +28,12 @@ SWITCH = "https://www.amazon.ca/s?i=videogames&bbn=8929975011&rh=n%3A8929975011%
 XBOX_ONE = "https://www.amazon.ca/s?i=videogames&bbn=8929975011&rh=n%3A8929975011%2Cn%3A3198031%2Cn%3A7089610011%2Cn%3A6920196011&dc&qid=1621288993&rnid=8929975011&ref=sr_nr_n_2&_encoding=UTF8&tag=awdeals04-20&linkCode=ur2&linkId=67c919358e64dfac3554553a359cde0e&camp=15121&creative=330641"
 XBOX_SERIES = "https://www.amazon.ca/s?i=videogames&bbn=8929975011&rh=n%3A8929975011%2Cn%3A3198031%2Cn%3A20974877011%2Cn%3A20974893011&s=price-desc-rank&dc&qid=1621898797&rnid=8929975011&ref=sr_nr_n_3&_encoding=UTF8&tag=awdeals04-20&linkCode=ur2&linkId=67c919358e64dfac3554553a359cde0e&camp=15121&creative=330641"
 
+ps4_no_referral = "https://www.amazon.ca/s?i=videogames&bbn=8929975011&rh=n%3A8929975011%2Cn%3A3198031%2Cn%3A7089437011%2Cn%3A6458584011&dc&qid=1613426168&rnid=8929975011&ref=sr_nr_n_2"
+ps5_no_referral = "https://www.amazon.ca/s?i=videogames&bbn=8929975011&rh=n%3A8929975011%2Cn%3A3198031%2Cn%3A20974860011%2Cn%3A20974876011&dc&qid=1614274309&rnid=8929975011&ref=sr_nr_n_2"
+switch_no_referral = "https://www.amazon.ca/s?i=videogames&bbn=8929975011&rh=n%3A8929975011%2Cn%3A3198031%2Cn%3A16329248011%2Cn%3A16329255011&dc&qid=1621288946&rnid=8929975011&ref=sr_nr_n_3"
+xbox_one_no_referral = "https://www.amazon.ca/s?i=videogames&bbn=8929975011&rh=n%3A8929975011%2Cn%3A3198031%2Cn%3A7089610011%2Cn%3A6920196011&dc&qid=1621288993&rnid=8929975011&ref=sr_nr_n_2"
+series_x_no_referral = "https://www.amazon.ca/s?i=videogames&bbn=8929975011&rh=n%3A8929975011%2Cn%3A3198031%2Cn%3A20974877011%2Cn%3A20974893011&s=price-desc-rank&dc&qid=1621898797&rnid=8929975011&ref=sr_nr_n_3"
+
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
@@ -55,19 +61,19 @@ class ContactForm(FlaskForm):
 
 def checker_thread():
     while True:
-        gc.initialize_webpages(PS4_URL, "PlayStation 4")
+        gc.initialize_webpages(ps4_no_referral, "PlayStation 4")
         price_data()
         time.sleep(151)
-        gc.initialize_webpages(PS5_URL, "PlayStation 5")
+        gc.initialize_webpages(switch_no_referral, "Nintendo Switch")
         price_data()
         time.sleep(151)
-        gc.initialize_webpages(XBOX_ONE, "Xbox One")
+        gc.initialize_webpages(ps5_no_referral, "PlayStation 5")
         price_data()
         time.sleep(151)
-        gc.initialize_webpages(XBOX_SERIES, "Xbox Series X")
+        gc.initialize_webpages(xbox_one_no_referral, "Xbox One")
         price_data()
         time.sleep(151)
-        gc.initialize_webpages(SWITCH, "Nintendo Switch")
+        gc.initialize_webpages(series_x_no_referral, "Xbox Series X")
         price_data()
         time.sleep(151)
         print("\nDone with this round\n")
