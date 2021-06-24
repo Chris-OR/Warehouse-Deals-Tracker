72c36f88b06efdbca66df33ca2396ec4c7999b36
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2021-06-24 10:06:37 -0600

    print out every active post on each round

--- game_checker.py
+++ game_checker.py
@@ -293,9 +293,10 @@ def initialize_webpages(url, console):
     else:
         print("uh oh")
 
-    # active_posts = ActivePosts.query.all()
-    # for post in active_posts:
-    #     print(f"{post.title} is currently an active post")
+    active_posts = ActivePosts.query.all()
+    for post in active_posts:
+        print(f"{post.title} is currently an active post")
+
     print("moving on to next console...")
 
 
