f8fffe02e8411d7c55121eae704d11c47c95c2ed
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2022-03-29 15:08:15 -0600

    update browser headers

--- game_checker.py
+++ game_checker.py
@@ -38,7 +38,7 @@ reddit = praw.Reddit(client_id=client_id,
                      user_agent=user_agent)
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/90.0.4430.212 Safari/537.36",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
     "Accept-Encoding": "gzip,deflate,br",
     "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
     "Accept-Language": "en-US,en;q=0.9",
@@ -194,7 +194,6 @@ def initialize_webpages(url, console):
         captcha_link = webpage_soup.find(name="img").get("src")
         captcha = AmazonCaptcha.fromlink(captcha_link)
         solution = captcha.solve()
-        print(captcha_link)
         print(solution)
         print("caught a captcha - we will move on")
         captcha = True
