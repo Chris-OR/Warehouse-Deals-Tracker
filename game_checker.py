b6e006d5a66ea9cd551e1bb1a1ebd2b2f62f28e7
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2022-05-05 15:20:47 -0600

    drop telegram users databases to begin migration with proper schema

--- game_checker.py
+++ game_checker.py
@@ -145,9 +145,9 @@ class SwitchTelegramUsers(db.Model):
     unsubscribed_games = db.Column(MutableList.as_mutable(db.PickleType), default=[])
 
 
-PSTelegramUsers.__table__.drop()
-XboxTelegramUsers.__table__.drop()
-SwitchTelegramUsers.__table__.drop()
+PSTelegramUsers.__table__.drop(db.engine)
+XboxTelegramUsers.__table__.drop(db.engine)
+SwitchTelegramUsers.__table__.drop(db.engine)
 
 # db.create_all()
 # db.session.commit()
