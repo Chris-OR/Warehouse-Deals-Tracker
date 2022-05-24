7ae149eaa94ecc47842b3990185103b23bc39c8e
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2022-05-24 16:11:21 -0600

    update header

--- game_checker.py
+++ game_checker.py
@@ -44,9 +44,9 @@ reddit = praw.Reddit(client_id=client_id,
                      user_agent=user_agent)
 
 headers = {
-    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:98.0) Gecko/20100101 Firefox/98.0",
+    "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:100.0) Gecko/20100101 Firefox/100.0",
     "Accept-Encoding": "gzip,deflate,br",
-    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,*/*;q=0.8",
     "Accept-Language": "en-US,en;q=0.9",
     "DNT": "1",
     "Connection": "close",
