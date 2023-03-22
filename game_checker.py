9f6289d50b442e938d6a89373997718f50b1d578
Chris-OR <32755139+Chris-OR@users.noreply.github.com>
2023-03-22 13:57:57 -0600

    update headers

--- game_checker.py
+++ game_checker.py
@@ -188,7 +188,7 @@ def get_headers():
         'Accept-Encoding': 'gzip, deflate, br',
         'Accept-Language': 'en-US,en;q=0.5',
         'Connection': 'keep-alive',
-        'Referer': 'https://www.example.com/',
+        'Referer': 'https://www.google.com/',
         'Upgrade-Insecure-Requests': '1'
     }
     return headers
